9009f0b6612173e3391ac90ae172f7501eeecb8d
Afzali <Hares.Afzali@kalo.de>
2023-11-21 18:05:57 +0100

    init commit

---
+++ src/main/java/com/tastybot/tastybot/TastyBotApplication.java
@@ -0,0 +1,13 @@
+package com.tastybot.tastybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tastybot/tastybot/TastyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tastybot.tastybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
