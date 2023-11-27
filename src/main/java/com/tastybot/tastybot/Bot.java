fc7661c1f7c259e422b9eb620d8a9ab09279332b
Afzali <Hares.Afzali@kalo.de>
2023-11-27 13:03:24 +0100

    Adding EventListener-Class to work with Discord-Slash-Commands, Update Bot-Class

--- src/main/java/com/tastybot/tastybot/Bot.java
+++ src/main/java/com/tastybot/tastybot/Bot.java
@@ -2,24 +2,38 @@ package com.tastybot.tastybot;
 //Diese Klasse dient zum starten des Discord-Bots
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service //Annotation Bean erkennen Application.context Spring erstellt bein Start Beans und speichert diese im Application.Context
-@Data //Setter/Getter etc.
+@Service
+/*Eine Annotation in Spring, die kennzeichnet, dass diese Klasse
+als Service-Klasse behandelt werden soll.
+Spring erkennt sie als Bean und fügt sie dem Anwendungscontext(application.context) hinzu.*/
+@Data
+/*Eine Lombok-Annotation, die automatisch Standardmethoden
+wie Getter, Setter, equals, hashCode und toString generiert.*/
+@RequiredArgsConstructor
 public class Bot {
 
     @Value("${discord_token}")
-    private String botToken;
+    /*Die @Value-Annotation in Spring ermöglicht es, Werte direkt in Felder von Spring-Komponenten
+    (wie Klassen, Methoden oder Felder in Konfigurationsklassen) zu injizieren.
+    Diese Werte können entweder aus den properties, Umgebungsvariablen oder anderen
+    Quellen stammen.*/
+    private String botToken; /*Verwendet Spring und setzt den Wert von botToken
+    aus einer Konfigurationsdatei oder Umgebungsvariable.*/
 
-    private JDA jda;
+    private JDA jda; /* Eine Instanzvariable vom Typ JDA, die die Instanz des Discord-Bots repräsentiert.*/
+
+    private final EventListener eventListener;
 
     public JDA startBot() throws InterruptedException {
         JDA jda = JDABuilder.createDefault(botToken)
-                .addEventListeners() //für die slash_commands
+                .addEventListeners(eventListener) //für die slash_commands
                 .setActivity(Activity.listening("Haftbefehl"))
                 .build()
                 .awaitReady();
@@ -28,7 +42,8 @@ public class Bot {
         return jda;
     }
 }
-//TODO: Eventlisteners alg. und Discord Bots informieren JAVA/JDA
+
 //TODO: Bisherigen Code erklären - wieso, weshalb, warum?
-//TODO: Git pushen
-//TODO: Dokumentation
+//TODO: Sequenz oder Aktivitätsdiagramm - Parallel zum Klassendiagramm.
+//TODO: Was ist Dependency Injection - Unterschiede erklären und Einsatzzwecke.
+//TODO: Nutzer kann über Slash-Commands Angebote erstellen und diese werden in der DB gespeichert.
---
+++ src/main/java/com/tastybot/tastybot/EventListener.java
@@ -0,0 +1,111 @@
+package com.tastybot.tastybot;
+
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+@RequiredArgsConstructor
+public class EventListener extends ListenerAdapter {
+
+    @Override //Server
+    public void onGuildReady(GuildReadyEvent event) {
+        ArrayList<CommandData> commandDataArrayList = new ArrayList<>();
+        commandDataArrayList.add(Commands.slash("erstelle-angebot", "Erstelle ein Angebot")
+                .addOptions(
+                        new OptionData(OptionType.STRING, "angebotstitel", "Hier kommt dein Angebotstitel hin. Beispiel: '3 Brötchen in Altona'", true),
+                        new OptionData(OptionType.BOOLEAN, "vegan", "Vegan oder nicht vegan?", true),
+                        new OptionData(OptionType.STRING, "verfuegbar-ab", "Ab wann ist das Angebot verfügbar? Bitte gib ein Datum an im Format dd.MM.yyyy", true),
+                        new OptionData(OptionType.STRING, "verfuegbar-bis", "Bis wann ist das Angebot verfügbar? Bitte gib ein Datum an im Format dd.MM.yyyy", true),
+                        new OptionData(OptionType.STRING, "anmerkungen", "Gibt es etwas zu beachten? Beispiel: '3. Stock ohne Fahrstuhl'", true),
+                        new OptionData(OptionType.STRING, "abholungsort", "Wo kann man dein Angebot abholen?", true)
+                                .addChoice("hamburg-ost", "hamburg-ost")
+                                .addChoice("hamburg-west", "hamburg-west")
+                                .addChoice("hamburg-sued", "hamburg-süd")
+                                .addChoice("hamburg-nord", "hamburg-nord")
+                                .addChoice("hamburg-mitte", "hamburg-mitte")
+                )
+        );
+
+        event.getGuild().updateCommands().addCommands(commandDataArrayList).queue();
+    }
+
+    @Override //Slash-Command-Events
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if(event.getName().equals("erstelle-angebot")) {
+
+            //angebotstitel
+            OptionMapping angebotstitelOption = event.getOption("angebotstitel");
+            assert angebotstitelOption != null;
+            String angebotstitel = angebotstitelOption.getAsString();
+
+            //vegan
+            OptionMapping veganOption = event.getOption("vegan");
+            assert veganOption != null;
+            Boolean vegan = veganOption.getAsBoolean();
+
+            //verfugbar ab
+            OptionMapping verfuegbarAbOption = event.getOption("verfuegbar-ab");
+            assert verfuegbarAbOption != null;
+            String verfuegbarAb = verfuegbarAbOption.getAsString();
+
+            if (!validateDate(verfuegbarAb)) {
+                event.reply("Das Datum muss im Format dd.MM.yyyy sein! Bitte versuche es erneut.").setEphemeral(true).queue();
+                return;
+            }
+
+            //verfugbar bis
+            OptionMapping verfuegbarBisOption = event.getOption("verfuegbar-bis");
+            assert verfuegbarBisOption != null;
+            String verfuegbarBis = verfuegbarBisOption.getAsString();
+
+            if (!validateDate(verfuegbarBis)) {
+                event.reply("Das Datum muss im Format dd.MM.yyyy sein! Bitte versuche es erneut.").setEphemeral(true).queue();
+                return;
+            }
+
+            //anmerkungen
+            OptionMapping anmerkungenOption = event.getOption("anmerkungen");
+            assert anmerkungenOption != null;
+            String anmerkungen = anmerkungenOption.getAsString();
+
+            //abholungsort
+            OptionMapping abholungsortOption = event.getOption("abholungsort");
+            assert abholungsortOption != null;
+            String abholungsort = abholungsortOption.getAsString();
+
+            System.out.println(
+                    "Angebotstitel: " + angebotstitel + "\n" +
+                    "Vegan: " + vegan + "\n" +
+                    "Verfügbar ab: " + verfuegbarAb + "\n" +
+                    "Verfügbar bis: " + verfuegbarBis + "\n" +
+                    "Anmerkungen: " + anmerkungen + "\n" +
+                    "Abholungsort: " + abholungsort
+            );
+
+            event.reply("JUHU😁 TastyBot hat dein Angebot erfolgreich angenommen!").setEphemeral(true).queue();
+        }
+
+    }
+
+    private boolean validateDate(String date) {
+        if (date.length() == 10) {
+            if (Integer.parseInt(date.substring(0, 2)) < 32 && Integer.parseInt(date.substring(0, 2)) > 0) {
+                if (Integer.parseInt(date.substring(3, 5)) < 13 && Integer.parseInt(date.substring(3, 5)) > 0) {
+                    return Integer.parseInt(date.substring(6, 10)) > 2022;
+                }
+            }
+        }
+        return false;
+    }
+}
